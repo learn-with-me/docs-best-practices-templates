d0d3232a4facdacaa76324f9ddf539f963113391
Anshul Goel <anshul.goel@onepeloton.com>
2022-12-24 00:17:21 -0500

    added script

---
+++ scripts/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Script to setup local virtual environment
+
+deactivate
+
+if [ ! -d "./scripts" ]
+then
+    virtualenv venv
+fi
+
+source venv/bin/activate
+pip install -r requirements.txt
+mkdocs serve
